e248e8d93f7bf125da663feb73a11f4a79550bfe
João Freitas <jvbf97@gmail.com>
2023-03-15 17:55:10 -0300

    created get_summary function

--- descriptive statistics.py
+++ descriptive statistics.py
@@ -1,4 +1,24 @@
 import pandas as pd
+import numpy as np
 
 
+def get_summary(numerical_attributes):
+    
+    # Central Tendency - mean, meadina 
+    ct1 = pd.DataFrame( numerical_attributes.apply( np.mean ) ).T
+    ct2 = pd.DataFrame( numerical_attributes.apply( np.median ) ).T
+
+    # dispersion - std, min, max, range, skew, kurtosis
+    d1 = pd.DataFrame( numerical_attributes.apply( np.std ) ).T 
+    d2 = pd.DataFrame( numerical_attributes.apply( min ) ).T 
+    d3 = pd.DataFrame( numerical_attributes.apply( max ) ).T 
+    d4 = pd.DataFrame( numerical_attributes.apply( lambda x: x.max() - x.min() ) ).T 
+    d5 = pd.DataFrame( numerical_attributes.apply( lambda x: x.skew() ) ).T 
+    d6 = pd.DataFrame( numerical_attributes.apply( lambda x: x.kurtosis() ) ).T 
+
+    # concatenar
+    m = pd.concat( [d2, d3, d4, ct1, ct2, d1, d5, d6] ).T.reset_index()
+    m.columns = ['attributes', 'min', 'max', 'range', 'mean', 'median', 'std', 'skew', 'kurtosis']
+    
+    return m
 
