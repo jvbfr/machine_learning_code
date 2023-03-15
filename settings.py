94ed818d9748898af2f4184ef6aaa4468d16c3b5
João Freitas <jvbf97@gmail.com>
2023-03-15 17:54:56 -0300

    updated jupyter settings

--- settings.py
+++ settings.py
@@ -2,13 +2,25 @@ def jupyter_settings():
     %matplotlib inline
     %pylab inline
     
-    plt.style.use( 'bmh' )
-    plt.rcParams['figure.figsize'] = [25, 12]
+    plt.style.use( 'fivethirtyeight' )
+    plt.rcParams['figure.figsize'] = [20, 8]
     plt.rcParams['font.size'] = 24
+    plt.rcParams['font.family'] = 'serif'
+    plt.rcParams['font.serif'] = 'Ubuntu'
+    plt.rcParams['font.monospace'] = 'Ubuntu Mono'
+    plt.rcParams['font.size'] = 12
+    plt.rcParams['axes.labelsize'] = 14
+    plt.rcParams['axes.labelweight'] = 'bold'
+    plt.rcParams['axes.titlesize'] = 15
+    plt.rcParams['xtick.labelsize'] = 10
+    plt.rcParams['ytick.labelsize'] = 10
+    plt.rcParams['legend.fontsize'] = 12
+    plt.rcParams['figure.titlesize'] = 12
     
-    display( HTML( '<style>.container { width:70% !important; }</style>') )
+    display( HTML( '<style>.container { width:80% !important; }</style>') )
     pd.options.display.max_columns = None
     pd.options.display.max_rows = None
     pd.set_option( 'display.expand_frame_repr', False )
     
     sns.set()
+    sns.set_style('ticks')
