09fba1a01254faeb44e42ce196c59755255d3597
João Freitas <jvbf97@gmail.com>
2023-03-15 17:51:02 -0300

    build docstring for cramer's V function

--- bivariate_analysis.py
+++ bivariate_analysis.py
@@ -1,6 +1,29 @@
 import numpy as np
+from scipy import stats  as ss
 
 def cramer_v( x, y ):
+        """
+    Calculate Cramer's V statistic for two categorical variables.
+
+    Parameters:
+    -----------
+    x: pandas.Series
+        A categorical variable represented as a pandas.Series.
+    y: pandas.Series
+        A categorical variable represented as a pandas.Series.
+
+    Returns:
+    --------
+    float
+        The Cramer's V statistic for the two variables.
+
+    Notes:
+    ------
+    Cramer's V statistic is a measure of association between two categorical variables.
+    It ranges between 0 (no association) and 1 (perfect association). This implementation
+    uses a corrected version of the chi-squared test and takes into account the number
+    of rows and columns of the contingency table to correct for bias.
+    """
     cm = pd.crosstab( x, y ).as_matrix()
     n = cm.sum()
     r, k = cm.shape
