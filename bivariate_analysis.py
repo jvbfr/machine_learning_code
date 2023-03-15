cdff7e40ed7b26f55d1427b98b802e9d3869c1ad
João Freitas <jvbf97@gmail.com>
2023-03-15 18:53:42 -0300

    included z test for two proportions

--- bivariate_analysis.py
+++ bivariate_analysis.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import scipy.stats as ss
-
+import scipy.ss as ss
 
 def cramer_v( x, y ):
         """
@@ -110,6 +109,28 @@ def plot_correlation_matrix(df):
     
     # Display the plot
     plt.show()
+
+def test_two_proportions(prop1, prop2, n1, n2, alpha=0.05):
+    """
+    Perform hypothesis testing to test if two proportions are statistically different or not.
+    
+    Parameters:
+        prop1 (float): The proportion of successes in the first sample.
+        prop2 (float): The proportion of successes in the second sample.
+        n1 (int): The size of the first sample.
+        n2 (int): The size of the second sample.
+        alpha (float): The significance level of the test (default is 0.05).
+    
+    Returns:
+        (result, p_value): A tuple containing the result of the test (True if the null hypothesis is rejected, 
+        False otherwise) and the p-value of the test.
+    """
+    p = (prop1*n1 + prop2*n2) / (n1 + n2)
+    z_score = (prop1 - prop2) / np.sqrt(p*(1-p)*(1/n1 + 1/n2))
+    p_value = 2 * (1 - ss.norm.cdf(abs(z_score)))
+    result = p_value < alpha
+    
+    return (result, p_value)
 
 
 def plot_scatter(df, x_col, y_col):
